60276593237d08d636e3b8cff930c99915ce49ce
isaacmodeyin <modeyinisaac6@gmail.com>
2023-10-09 10:43:05 +0100

    New files added & git ignore

--- Basics/template.js
+++
@@ -1,9 +0,0 @@
-let nme = 'ISAAC'
-const Adj= 'a programmer</>'
-
-// nme+=Adj
-// console.log(nme);
-
-outPu = `my name is ${nme} and I am ${Adj}`
-
-console.log(outPu)
---
+++ Personal/template.js
@@ -0,0 +1,40 @@
+let nme = 'ISAAC'
+const Adj= 'a programmer</>'
+
+// nme+=Adj
+// console.log(nme);
+
+outPu = `my name is ${nme} and I am ${Adj}`
+
+console.log(outPu)
+
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+
+let
+nam = "ISAAC";
+
+nam = "I changed my name"
+
+console.log(nam)
+
+let ame;
+let surname;
+let middlename;
+let father;
+
+ame = "samuel";
+surname = "olobayo"
+middlename = "olowotubi"
+father = "okuta"
+
+console.log({ame,surname,middlename,father})
+
+let obj = {ame, surname, middlename, father};
+
+console.table(obj);
